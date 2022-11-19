58d253ce1f171ee49008d8f6a72beebfd98d32d0
Ksenia <Kseniakuzevanova@gmail.com>
2022-11-19 19:13:23 +0700

    точки пересечения и М чесло

--- Program.cs
+++ Program.cs
@@ -1,45 +1,50 @@
 ﻿// Пользователь вводит с клавиатуры M чисел. 
 // Посчитайте, сколько чисел больше 0 ввёл пользователь
-// int[] array = new int[5];
-// FillArray(array);
-// int result = FindPositiveNumbers(array);
-// Console.Write($"Количество чисел больше 0: {result} ");
+int[] array = new int[5];
+FillArray(array);
+int result = FindPositiveNumbers(array);
+Console.Write($"Количество чисел больше 0: {result} ");
 
-// //заполняем массив
-// void FillArray(int[] array)
-// {
-//     Console.WriteLine("введите 5 чисел: ");
-//     for (var i = 0; i < array.Length; i++)
-//     {
-//         int numbers = Convert.ToInt32(Console.ReadLine());
-//     }
-// }
+//заполняем массив
+void FillArray(int[] array)
+{
+     
+    Console.WriteLine("введите 5 чисел: ");
+    for (var i = 0; i < array.Length; i++)
+    {
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
 
-// //находим числа > 0
-// int FindPositiveNumbers(int[] array)
-// {
-//     int count = 0;
+//находим числа > 0
+int FindPositiveNumbers(int[] array)
+{
+    int count = 0;
 
-//     for (int i = 0; i < array.Length; i++)
-//     {
-//         if (array[i] > 0)
-//         {
-//             count++;
-//         }
-//     }
-//     return count;
-// }
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
 
 // Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
 // y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 
 
 Console.Write("Введите k1: ");
-int k1 = Convert.ToInt32(Console.ReadLine());
+double k1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите b1: ");
-int b1 = Convert.ToInt32(Console.ReadLine());
+double b1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите k2: ");
-int k2 = Convert.ToInt32(Console.ReadLine());
+double k2 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите b2: ");
-int b2 = Convert.ToInt32(Console.ReadLine());
+double b2 = Convert.ToInt32(Console.ReadLine());
 
+double x = (b2 - b1)/(k1-k2);
+double y = k2*((b2 - b1)/(k1-k2)) + b2;
+ Console.WriteLine($"точка пересечения: {x} {y}");
+ 
